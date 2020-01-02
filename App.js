6d8eb284272f391848fc91605cd4e1a991b2598f
ardiwirawan11 <ardi_wirawan11@yahoo.com>
2020-01-02 13:04:50 +0700

    [Ardi] Update

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import configureStore from './store';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import HomeScreen from './src/components/HomeScreen';
-import MondayScreen from './src/components/MondayScreen';
-import TuesdayScreen from './src/components/TuesdayScreen';
-import WednesdayScreen from './src/components/WednesdayScreen';
-import ThursdayScreen from './src/components/ThursdayScreen';
-import FridayScreen from './src/components/FridayScreen';
-import SaturdayScreen from './src/components/SaturdayScreen';
-import SundayScreen from './src/components/SundayScreen';
+import React from 'react'
+import { Provider } from 'react-redux'
+import configureStore from './store'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import HomeScreen from './src/components/HomeScreen'
+import MondayScreen from './src/components/MondayScreen'
+import TuesdayScreen from './src/components/TuesdayScreen'
+import WednesdayScreen from './src/components/WednesdayScreen'
+import ThursdayScreen from './src/components/ThursdayScreen'
+import FridayScreen from './src/components/FridayScreen'
+import SaturdayScreen from './src/components/SaturdayScreen'
+import SundayScreen from './src/components/SundayScreen'
 
 export default class App extends React.Component {
   static navigationOptions = {
@@ -23,7 +23,7 @@ export default class App extends React.Component {
       <Provider store={store}>
         <AppContainer />
       </Provider>
-    );
+    )
   }
 }
 
@@ -40,7 +40,7 @@ const HomeStack = createStackNavigator({
   {
     initialRouteName: 'Home'
   }
-);
+)
 
-const AppContainer = createAppContainer(HomeStack);
+const AppContainer = createAppContainer(HomeStack)
 
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -97,4 +97,4 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen)
