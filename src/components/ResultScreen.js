0d1adbab479807572bdacc72a331e93eeb7ca61e
ardiwirawan11 <ardi_wirawan11@yahoo.com>
2020-01-06 17:21:14 +0700

    [Ardi] Update

--- src/components/ResultScreen.js
+++ src/components/ResultScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet, ScrollView } from 'react-native'
-import { Spinner, Container, Content, Button } from 'native-base'
+import { Spinner, Container, Button } from 'native-base'
 import { connect } from 'react-redux'
 
 class ResultScreen extends Component {
@@ -15,17 +15,17 @@ class ResultScreen extends Component {
     componentDidMount() {
         this.getDateData()
     }
- 
+
     processData = async (index1, index2) => {
-        var dataApi = await this.props.task.data
-        var filterDay = dataApi.filter(function (data) {
+        let dataApi = await this.props.task.data
+        let filterDay = dataApi.filter(function (data) {
             return data.day_of_week === index1 && data.active === 1
         })
         const sortedDay = filterDay.sort(function (a, b) {
             return parseInt(a.start_time, 10) - parseInt(b.start_time, 10)
         })
-        var availableTime = sortedDay.filter(function (data) {
-            var CurrentTime = new Date().getHours() + ':' + new Date().getMinutes()
+        let availableTime = sortedDay.filter(function (data) {
+            let CurrentTime = new Date().getHours() + ':' + new Date().getMinutes()
             if (data.day_of_week === new Date().getDay() & index2 === new Date().getDate()) {
                 return data.start_time > CurrentTime
             } else {
@@ -33,7 +33,7 @@ class ResultScreen extends Component {
             }
         })
         this.setState({ btnSelected: index2, availableTime: availableTime })
-        var highDiscount = availableTime.sort(function (a, b) {
+        let highDiscount = availableTime.sort(function (a, b) {
             return b.discount - a.discount && parseInt(a.start_time, 10) - parseInt(b.start_time, 10)
         })
         this.pickTime(highDiscount ? highDiscount[0].id : null)
@@ -44,39 +44,39 @@ class ResultScreen extends Component {
     }
 
     getDateData = async () => {
-        var now = new Date()
-        var dd = now.getDate() - 1
-        var mm = now.getMonth() + 1
-        var yyyy = now.getFullYear()
-        var startDate
-        var d
-        var output = []
+        let now = new Date()
+        let dd = now.getDate() - 1
+        let mm = now.getMonth() + 1
+        let yyyy = now.getFullYear()
+        let startDate
+        let currentDate
+        let output = []
 
-        startDate = yyyy + '-' + mm + '-' + dd
-        d = new Date(startDate)
-        for (var i = dd; i <= dd + 14; i++) {
-            d.setDate(d.getDate() + 1)
+        startDate = yyyy + '-' + (mm < 10 ? '0' + mm : mm) + '-' + (dd < 10 ? '0' + dd : dd)
+        currentDate = new Date(startDate)
+        for (let i = dd; i <= dd + 14; i++) {
+            currentDate.setDate(currentDate.getDate() + 1)
             output.push({
-                date: d.getDate(),
-                day: d.getDay(),
-                month: d.getMonth()
+                date: currentDate.getDate(),
+                day: currentDate.getDay(),
+                month: currentDate.getMonth()
             })
         }
-        var result = []
-        var dataApi = await this.props.task.data
-        var day = dataApi.filter(function (data) {
+        let result = []
+        let dataApi = await this.props.task.data
+        let day = dataApi.filter(function (data) {
             return data.active === 1
         })
-        for (var j = 0; j < day.length; j++) {
-            result.push(day[j].day_of_week)
+        for (let i = 0; i < day.length; i++) {
+            result.push(day[i].day_of_week)
         }
         let activeDay = [...new Set(result)];
         this.setState({ dateData: output, activeDay: activeDay })
-        var today = new Date().getDay()
+        let today = new Date().getDay()
         if (activeDay.includes(today)) {
             this.processData(today, new Date().getDate())
         } else {
-            for (var i = 1; i < 7; i++) {
+            for (let i = 1; i < 7; i++) {
                 if (today + i > 7 && activeDay.includes(today + i - 7)) {
                     this.processData(today + i - 7, new Date().getDate() + i)
                 } else if (today + i <= 7 && activeDay.includes(today + i)) {
@@ -92,27 +92,37 @@ class ResultScreen extends Component {
 
     render() {
         const dateList = this.state.dateData.map((item) => {
-            var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-            const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+            let dayNames = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+            const monthNames = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
             return (
-                <View key={item.date} style={styles.viewMap}>
+                <View key={item.date} style={styles.viewMapDate}>
                     {(this.state.activeDay).includes(item.day) ?
-                        <Button bordered style={(this.state.btnSelected === item.date) ? styles.btnSelected : styles.btnNotSelected}
+                        <Button style={(this.state.btnSelected === item.date) ? styles.btnSelected : styles.btnNonSelected}
                             onPress={() => this.processData(item.day, item.date)} >
-                            <Text>{dayNames[item.day]} </Text>
-                            <Text>{item.date} </Text>
-                            <Text>{monthNames[item.month]} </Text>
+                            <Text style={(this.state.btnSelected === item.date) ? styles.textDayMonthActive : styles.textDayMonthNonActive}>{dayNames[item.day]}</Text>
+                            <Text style={(this.state.btnSelected === item.date) ? styles.textDateActive : styles.textDateNonActive}>{item.date} </Text>
+                            <Text style={(this.state.btnSelected === item.date) ? styles.textDayMonthActive : styles.textDayMonthNonActive}>{monthNames[item.month]} </Text>
                         </Button> : null}
                 </View>
             )
         })
         const timeSlots = this.state.availableTime.map((item) => {
+            let splitTime = (item.start_time).split(':')
+            let joinTime
+            if (splitTime[0] < 12) {
+                joinTime = splitTime.join(':') + 'am'
+            } else if (splitTime[0] > 12) {
+                splitTime[0] = splitTime[0] - 12
+                joinTime = splitTime.join(':') + 'pm'
+            } else {
+                joinTime = splitTime.join(':') + 'pm'
+            }
             return (
                 <View key={item.id} style={styles.viewMapTime}>
-                    <Button bordered style={(this.state.btnActive === item.id) ? styles.btnActive : styles.btnInactive}
+                    <Button style={(this.state.btnActive === item.id) ? styles.btnActive : styles.btnNonActive}
                         onPress={() => this.pickTime(item.id)} >
-                        <Text>{item.start_time}</Text>
-                        <Text>-{item.discount}% </Text>
+                        <Text style={(this.state.btnActive === item.id) ? styles.textTimeActive : styles.textTimeNonActive}>{joinTime}</Text>
+                        <Text style={(this.state.btnActive === item.id) ? styles.textTimeDiscountActive : styles.textTimeDiscountNonActive}>-{item.discount}% </Text>
                     </Button>
                 </View>
             )
@@ -123,14 +133,14 @@ class ResultScreen extends Component {
                     <View style={styles.spinner}>
                         <Spinner></Spinner>
                     </View> :
-                    <Content>
-                        <ScrollView horizontal>
+                    <View>
+                        <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.sviewHorizontal}>
                             {dateList}
                         </ScrollView>
-                        <ScrollView horizontal>
+                        <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.sviewHorizontal}>
                             {timeSlots}
                         </ScrollView>
-                    </Content>
+                    </View>
                 }
             </Container>
         )
@@ -138,14 +148,18 @@ class ResultScreen extends Component {
 }
 
 const styles = StyleSheet.create({
+    sviewHorizontal:{
+        flexGrow: 1
+    },
     viewMapTime: {
         backgroundColor: '#F6F9F8',
-        justifyContent: 'center',
         marginVertical: 10,
         paddingVertical: 30,
-        paddingHorizontal: 5
+        paddingHorizontal: 5,
+        flex:1,
+        justifyContent:'flex-start'
     },
-    viewMap: {
+    viewMapDate: {
         justifyContent: 'center',
         marginVertical: 10,
         paddingVertical: 10
@@ -158,36 +172,87 @@ const styles = StyleSheet.create({
     btnSelected: {
         borderColor: '#FF3164',
         flexDirection: 'column',
-        borderRadius: 20,
+        borderRadius: 15,
+        borderWidth:1,
         marginHorizontal: 10,
         width: 50,
         height: 70,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        backgroundColor: '#FFFFFF'
     },
-    btnNotSelected: {
+    btnNonSelected: {
         marginHorizontal: 10,
         flexDirection: 'column',
-        borderRadius: 20,
+        borderWidth: 0,
+        borderRadius: 15,
         width: 50,
         height: 70,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        backgroundColor: '#F6F9F8'
     },
     btnActive: {
         backgroundColor: '#FF3164',
         flexDirection: 'column',
-        borderRadius: 20,
+        borderRadius: 10,
         marginHorizontal: 10,
-        width: 70,
-        height: 50,
+        width: 90,
+        height: 60,
         justifyContent: 'center'
     },
-    btnInactive: {
+    btnNonActive: {
         marginHorizontal: 10,
         flexDirection: 'column',
-        borderRadius: 20,
-        width: 70,
-        height: 50,
-        justifyContent: 'center'
+        borderRadius: 7,
+        backgroundColor: '#FFFFFF',
+        width: 90,
+        height: 60,
+        borderWidth:1,
+        justifyContent: 'center',
+        borderColor:'gray'
+    },
+    textDayMonthActive: {
+        fontSize: 12,
+        color: '#FF3164',
+        fontFamily: 'Roboto'
+    },
+    textDayMonthNonActive: {
+        fontSize: 12,
+        fontFamily: 'Roboto',
+        color:'gray'
+    },
+    textDateActive: {
+        fontSize: 20,
+        color: '#FF3164',
+        fontFamily: 'Roboto',
+        fontWeight: "bold"
+    },
+    textDateNonActive: {
+        fontSize: 20,
+        fontFamily: 'Roboto',
+        fontWeight: "bold",
+        color:'gray'
+    },
+    textTimeActive: {
+        fontSize: 16,
+        color:  '#F6F9F8',
+        fontFamily: 'Roboto'
+    },
+    textTimeNonActive: {
+        fontSize: 16,
+         color: '#FF3164',
+        fontFamily: 'Roboto'
+    },
+    textTimeDiscountActive: {
+        fontSize: 20,
+        color: '#FFFFFF',
+        fontFamily: 'Roboto',
+        fontWeight: "bold"
+    },
+    textTimeDiscountNonActive: {
+        fontSize: 20,
+         color: '#FF3164',
+        fontFamily: 'Roboto',
+        fontWeight: "bold"
     }
 })
 const mapStateToProps = state => ({
